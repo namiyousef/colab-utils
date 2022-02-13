a3be240ddc9747777587f261931a113b812a86da
Yousef <namiyousef@hotmail.com>
2022-02-13 19:06:52 +0000

    updates utils

--- colabutils/utils.py
+++ colabutils/utils.py
@@ -16,7 +16,7 @@ def mount_drive():
     base_path = f'/content/{DRIVE_NAME}'
     drive.mount(base_path)
     print('Google Drive mount successful.')
-    return base_path
+    return os.path.join(base_path, 'MyDrive')
 
 
 def install_package_dev_mode(repo_name, requirements='requirements.txt'):
