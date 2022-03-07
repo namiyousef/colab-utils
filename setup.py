6d5f8ce046e6046e346ce983f00e1eee41401107
Yousef <namiyousef@hotmail.com>
2022-03-07 20:19:42 +0000

    renames project to colabtools

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='colabutils',
+    name='colabtools',
     version='0.0.1',
     description='Python utils package for Google Colab',
     author='Yousef Nami',
     author_email='namiyousef@hotmail.com',
     url='https://github.com/namiyousef/colab-utils',
-    #install_requires=[],
+    install_requires=['nvidia-ml-py3'],
     #package_data={}
     packages=find_packages(exclude=('tests*', 'experiments*')),
     license='MIT',
