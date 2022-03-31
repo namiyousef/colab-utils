c774f8f9d1c5c23a19702c39be3a802dd9a43e39
Yousef <namiyousef@hotmail.com>
2022-03-31 17:42:35 +0200

    minor refactoring; adds CUDA device

--- colabtools/config.py
+++ colabtools/config.py
@@ -1,3 +1,18 @@
+# -- public imports
+import torch
 from os import environ
+try:
+    from google.colab import drive
+    print('Google Drive import successful.')
+except:
+    print('Google Drive import failed... continuing anyways...')
 
+# -- constants
 DRIVE_NAME = environ.get('DRIVE_NAME', 'drive')
+
+if torch.cuda.is_available():
+    DEVICE = 'cuda'
+    print('CUDA device detected. Using GPU...')
+else:
+    DEVICE = 'cpu'
+    print('CUDA device NOT detected. Using CPU...')
