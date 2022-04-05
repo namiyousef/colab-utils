aed8c5ca67fcd1dc864b32fa30c4eb43ac9577f0
Yousef <namiyousef@hotmail.com>
2022-04-05 14:21:59 +0100

    test commit for actions

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import subprocess
 import sys
 
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+def install_git(repo_link):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", f'git+{repo_link}'])
+
+def test():
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-e', '/Users/yousefnami/Desktop/Main/0.Education/2.UCL/Courses/NLP/argument-mining'])
 
 
-install('git@github.com:namiyousef/argument-mining.git')
+test()
