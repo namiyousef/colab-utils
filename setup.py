cd9eab1ba24ce4214fcf1f3a19d4bab05f7e3216
Yousef <namiyousef@hotmail.com>
2022-04-05 15:49:40 +0100

    updates configuration file to remove dependency from requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
+import os
+import sys
 from setuptools import setup, find_packages
 
-with open('requirements.txt', 'r') as f:
-    install_requires = f.read().split()
+#this_dir = os.path.abspath(os.path.dirname(__file__))
+#sys.path.insert(0, this_dir)
+
+#requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
+
+#with open(requirements_path, 'r') as f:
+#    install_requires = f.read().split()
 
 setup(
     name='colabtools',
@@ -10,7 +17,8 @@ setup(
     author='Yousef Nami',
     author_email='namiyousef@hotmail.com',
     url='https://github.com/namiyousef/colab-utils',
-    install_requires=install_requires,
+    #install_requires=install_requires,
+    install_requires=['torch', 'numpy', 'nvidia-ml-py3'],
     #package_data={}
     packages=find_packages(exclude=('tests*', 'experiments*')),
     license='MIT',
